cdfe7b169dbb49922775350ae3508b2a22296b28
Sutulov Kirill <kirillsutulov228@mail.ru>
2022-06-20 19:50:40 +0400

    added main page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
 const app = express();
 
 const PORT = +process.env.PORT ?? 8000;
@@ -13,7 +14,16 @@ app.set("layout extractScripts", true)
 app.set("layout extractStyles", true);
 
 app.use('/public', express.static('./public'));
-app.use(expressLayouts);
+app.use(expressLayouts, session({
+  secret: process.env.SESSION_SECRET
+}));
+
+app.use((req, res, next) => {
+  if (req.session.user) {
+    res.locals.user = req.session.user;
+  }
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('pages/index', { title: "Верный Друг" });
